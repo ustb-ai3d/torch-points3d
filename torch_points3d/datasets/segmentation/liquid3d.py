5e70874af4473c3532c53ef901069c3eedc467df
dfldylan <dfldylan@qq.com>
2023-12-27 12:10:34 +0800

    feat: add gen_liquid3d_raw for temp

--- torch_points3d/datasets/segmentation/liquid3d.py
+++ torch_points3d/datasets/segmentation/liquid3d.py
@@ -15,7 +15,14 @@ from torch_points3d.metrics.shapenet_part_tracker import ShapenetPartTracker
 from torch_points3d.datasets.base_dataset import BaseDataset, save_used_properties
 from torch_points3d.utils.download import download_url
 
+def gen_liquid3d_raw():
+    # 大概的数据结构，每执行一次本函数，取出一个【输入+输出】
+    # in_pos: [points_num, 3] 每个点的坐标
+    # in_feats: [points_num, feats_channel] 每个点的特征。如果模型不支持输入特征，则忽略该向量
+    # out: [points_num, 3] 每个点的输出。原始点云分割任务中是[points_num, 16]，对应每个点的16个通道代表16个类的概率预测。在这里我们只输出3个通道，可能是速度也可能是加速度的xyz值。
 
+    in_pos, in_feats,out = torch.ones((None, 3)),torch.ones((None, 3)),torch.ones((None, 3)) # 随便写的
+    return in_pos, in_feats, out
 
 class ShapeNet(InMemoryDataset):
     r"""The ShapeNet part level segmentation dataset from the `"A Scalable
